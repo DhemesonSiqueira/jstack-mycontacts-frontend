9211b03507e1fdac332dca4d13187d9007afab97
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2023-01-14 23:31:54 -0300

    feat: funcionalidade de remover toasts

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -5,21 +5,24 @@ import { Container } from './styles';
 import xCircleIcon from '../../../assets/images/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/check-circle.svg';
 
-export default function ToastMessage({ text, type }) {
+export default function ToastMessage({ message, onRemoveMessage }) {
+  function handleRemoveToast() {
+    onRemoveMessage(message.id);
+  }
   return (
-    <Container type={type}>
-      {type === 'success' && <img src={checkCircleIcon} alt="Success" />}
-      {type === 'danger' && <img src={xCircleIcon} alt="X" />}
-      <strong>{text}</strong>
+    <Container type={message.type} onClick={handleRemoveToast}>
+      {message.type === 'success' && <img src={checkCircleIcon} alt="Success" />}
+      {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
+      <strong>{message.text}</strong>
     </Container>
   );
 }
 
 ToastMessage.propTypes = {
-  text: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['default', 'success', 'danger']),
-};
-
-ToastMessage.defaultProps = {
-  type: 'default',
+  message: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['default', 'success', 'danger']),
+  }).isRequired,
+  onRemoveMessage: PropTypes.func.isRequired,
 };
