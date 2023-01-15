760262e490a4722afe1b26db175e06ef5d77c195
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2023-01-15 14:13:11 -0300

    feat: removendo toasts automaticamente

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Container } from './styles';
@@ -6,11 +7,28 @@ import xCircleIcon from '../../../assets/images/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/check-circle.svg';
 
 export default function ToastMessage({ message, onRemoveMessage }) {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      console.log('Time executou');
+      onRemoveMessage(message.id);
+    }, message.duration || 5000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [message, onRemoveMessage]);
+
   function handleRemoveToast() {
     onRemoveMessage(message.id);
   }
+
   return (
-    <Container type={message.type} onClick={handleRemoveToast}>
+    <Container
+      type={message.type}
+      onClick={handleRemoveToast}
+      tabIndex={0}
+      role="button"
+    >
       {message.type === 'success' && <img src={checkCircleIcon} alt="Success" />}
       {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
       <strong>{message.text}</strong>
@@ -23,6 +41,7 @@ ToastMessage.propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['default', 'success', 'danger']),
+    duration: PropTypes.number,
   }).isRequired,
   onRemoveMessage: PropTypes.func.isRequired,
 };
